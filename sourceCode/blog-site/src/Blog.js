8af0364adfcdc89b2e7067cd5d1f655fcb52b990
bkalish <benjamin.kalish@gmail.com>
2024-02-06 07:57:11 -0500

    blog link update index text update

--- sourceCode/blog-site/src/Blog.js
+++ sourceCode/blog-site/src/Blog.js
@@ -22,7 +22,7 @@ export default function Blog(props) {
     return (<section className={`blogBox ${className}`} onClick={(e) => blogSelector(e, id)}>
         <h2>{blogName}</h2>
         <h4>by {username}</h4>
-        <a href="https://pcsnynj.org/course/software-web-and-app-development/" target="_blank" rel="noreferrer">{website}</a>
+        <a href="https://benjaminkalish.github.io/" target="_blank" rel="noreferrer">{website}</a>
         <div className="postCount">
             <div>{postCount && 'Posts'}</div>
             <div>{postCount}</div>
